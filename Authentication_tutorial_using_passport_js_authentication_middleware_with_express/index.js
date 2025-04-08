9876825d21a65f865d7871fbbac52ba0275e6ac2
Javier Ruiz <javier.ruiz.agustin@estudiantat.upc.edu>
2025-04-08 16:57:11 +0200

    Oauth part added

--- Authentication_tutorial_using_passport_js_authentication_middleware_with_express/index.js
+++ Authentication_tutorial_using_passport_js_authentication_middleware_with_express/index.js
@@ -1,11 +1,11 @@
 'use strict';
-
+const session = require('express-session');
 const express = require('express');
 const logger = require('morgan');
 const https = require('https');
 const fs = require('fs');
 const { verify } = require('scrypt-mcf');
-
+require('dotenv').config();
 const tlsServerKey = fs.readFileSync('./tls/webserver.key.pem');
 const tlsServerCrt = fs.readFileSync('./tls/webserver.crt.pem');
 const jwt = require('jsonwebtoken')
@@ -14,6 +14,7 @@ const LocalStrategy = require('passport-local').Strategy
 const cookieParser = require('cookie-parser');
 const jwtSecret = require('crypto').randomBytes(16)
 const JwtStrategy = require('passport-jwt').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 
 const app = express();
@@ -22,9 +23,18 @@ app.use(cookieParser());
 
 let users = [];  
 
+app.use(session({
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }  
+}));
+
+app.use(passport.session());
+
 try {
-  const data = fs.readFileSync('./users_slow.json', 'utf-8'); 
-  //const data = fs.readFileSync('./users_fast.json', 'utf-8'); 
+  //const data = fs.readFileSync('./users_slow.json', 'utf-8'); 
+  const data = fs.readFileSync('./users_fast.json', 'utf-8'); 
   users = JSON.parse(data); 
   if (!Array.isArray(users)) throw new Error("Invalid JSON format: Expected an array");
 } catch (error) {
@@ -60,23 +70,46 @@ try {
   }
   ));
 
+  passport.use('google', new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+    ));
+
+  passport.serializeUser((user, done) => {
+      done(null, user); // Serializa el objeto de perfil completo
+    });
+    
+  passport.deserializeUser((user, done) => {
+      done(null, user); // Devuelve el usuario completo en la sesión
+    });
+    
+
+
   passport.use('jwtCookie', new JwtStrategy(
-    {
-      jwtFromRequest: (req) => req?.cookies?.jwt || null,
-      secretOrKey: jwtSecret
-    },
-    (jwtPayload, done) => {
-      const user = users.find(u => u.username === jwtPayload.sub);
-      if (user) {
+      {
+        jwtFromRequest: (req) => req?.cookies?.jwt || null,
+        secretOrKey: jwtSecret
+      },
+      (jwtPayload, done) => {
+        // Acepta cualquier usuario con JWT válido
         return done(null, { username: jwtPayload.sub, role: jwtPayload.role || 'user' });
       }
-      return done(null, false);
-    }
-  ));
+    ));
+    
   
 app.use(express.urlencoded({ extended: true })) 
 app.use(passport.initialize()) 
+app.use(passport.session());
 
+app.get('/auth/google', (req, res, next) => {
+  console.log("Google Auth route accessed");
+  next(); 
+}, passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 
 app.get('/',
@@ -85,13 +118,34 @@ app.get('/',
       { session: false, failureRedirect: '/login' }
     ),
     (req, res) => {
+      console.log(req)
       res.send(`Welcome to your private page, ${req.user.username}!`) 
     }
   )
 
 app.get('/login',(req, res) => {
     res.sendFile('login.html', { root: __dirname }) 
-  })  
+   })  
+
+app.get('/auth/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/login' , session: false }),
+    function (req, res) {
+      console.log(req.user)
+      const jwtClaims = {
+        sub: req.user.displayName,
+        iss: 'localhost:3000',
+        aud: 'localhost:3000',
+        exp: Math.floor(Date.now() / 1000) + 604800, 
+        role: 'user' 
+      }
+  
+      const token = jwt.sign(jwtClaims, jwtSecret)
+      console.log(token)
+      res.cookie('jwt', token, { httpOnly: true, secure: true }) 
+      res.redirect('/');
+
+    }
+  );
 
 app.post('/login',
     passport.authenticate('username-password', { failureRedirect: '/login', session: false }), 
@@ -113,6 +167,7 @@ app.post('/login',
       //console.log(`Token secret (for verifying the signature): ${jwtSecret.toString('base64')}`)
     }
   )
+
 
 app.get('/logout', (req, res) => {
     res.clearCookie('jwt');
